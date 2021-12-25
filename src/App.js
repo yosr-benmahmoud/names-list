e4b00fadd287c8feb55d6bfffaae8a29e97f2fa4
Ghassen bougacha <52534237+ghassen-create@users.noreply.github.com>
2021-12-25 12:14:59 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import TodoItems from './components/todoItems/todoItems'
+import AddItem from './components/addItem/addItem'
+
+class App extends Component {
+
+    state = {
+        items: [
+            
+        ]
+    }
+
+    deleteItem = (id) => {
+        let items = this.state.items.filter(item => {
+            return item.id !== id
+        })
+        this.setState({items})
+    }
+
+    addItem = (item) => {
+        item.id = Math.random()
+        let items = this.state.items
+        items.push(item)
+        this.setState({items})
+    }
+
+    render() {
+        return (
+            <div className="App container">
+                <h1 className="text-center">Names list</h1>
+                <TodoItems items={this.state.items} deleteItem={this.deleteItem} />
+                <AddItem addItem={this.addItem} />
+            </div>
+        )
+    }
+}
+
+export default App
